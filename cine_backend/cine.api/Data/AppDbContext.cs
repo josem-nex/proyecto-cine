c6dbaf7259bb2fc800021d58ed4b9a019609ae96
josem-nex <josem.zayas@estudiantes.matcom.uh.cu>
2024-02-04 21:50:37 -0500

    post and get user

---
+++ cine_backend/cine.api/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace cine.api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+    }
+}
