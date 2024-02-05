12d9dac25f772560898396e2def1cacf7cd5a3e8
josem-nex <josem.zayas@estudiantes.matcom.uh.cu>
2024-02-05 12:01:38 -0500

    add angular material

--- cine_frontend/src/app/app.component.ts
+++ cine_frontend/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'cine_frontend';
 }
+
