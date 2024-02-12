09bb18dc78bc10e5797783fea755c39d0fdd6bdc
nex <110191406+josem-nex@users.noreply.github.com>
2024-02-11 19:53:30 -0500

    fix directory cine_backend

--- cine_backend/cine.application/Services/Authentication/AuthenticationResult.cs
+++
@@ -1,8 +0,0 @@
-namespace Cine.Application.Services.Authentication;
-public record AuthenticationResult(
-    Guid ID, 
-    string Firstname, 
-    string Lastname, 
-    string Email, 
-    int Points, 
-    string Token);
--- cine_backend/cine.infrastructure/DependencyInjection.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.Extensions.DependencyInjection;
-
-namespace Cine.Infraestructure;
-public static class DependencyInjection{
-    public static IServiceCollection AddInfraestructure(this IServiceCollection services)
-    {
-        return services;
-    }
-}
