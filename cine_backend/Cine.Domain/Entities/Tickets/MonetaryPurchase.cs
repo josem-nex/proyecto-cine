24b9d6fb09208836c7ab9b8699ec3976de839b52
josem-nex <josem.zayas@estudiantes.matcom.uh.cu>
2024-03-31 18:26:02 -0400

    fix: userid, int -> Guid in purchases

--- cine_backend/Cine.Domain/Entities/Tickets/MonetaryPurchase.cs
+++ cine_backend/Cine.Domain/Entities/Tickets/MonetaryPurchase.cs
@@ -2,7 +2,7 @@ namespace Cine.Domain.Entities.Tickets;
 public class MonetaryPurchase
 {
     public User? User { get; private set; }
-    public int UserId { get; private set; }
+    public Guid UserId { get; private set; }
     public Ticket? Tickets { get; private set; }
     public int TicketsId { get; private set; }
     public int TotalPrice { get; private set; }
--- cine_backend/Cine.Domain/Entities/Tickets/PointsPurchase.cs
+++ cine_backend/Cine.Domain/Entities/Tickets/PointsPurchase.cs
@@ -2,7 +2,7 @@ namespace Cine.Domain.Entities.Tickets;
 public class PointsPurchase
 {
     public Partner? User { get; private set; }
-    public int UserId { get; private set; }
+    public Guid UserId { get; private set; }
     public Ticket? Tickets { get; private set; }
     public int TicketsId { get; private set; }
     public int TotalPoints { get; private set; }
